eebb27028eab24d9ee6b68cc66999b64c6e31f60
Shalma <sebcas@yahoo.fr>
2019-01-20 06:06:36 +0100

    ajout des points d'entrées du serveur node, pour requetage sur neo4j

--- src/server/requeteNeo4j.js
+++ src/server/requeteNeo4j.js
@@ -28,5 +28,96 @@ exports.getRecruteur = function(nbPersonne, res) {
     console.log(error)
   }); 
 }
+
+exports.getPays = function(res) {
+  let driver = createConnexion()
+  let session = driver.session()
+
+  let request = 'MATCH (n:pays) RETURN n'
+
+  session
+  .run(request)
+  .then(function (result) {
+    let resultat = []
+    result.records.forEach(function (record) {
+      resultat.push(record.get('n').properties)
+    });
+  res(resultat)
+  session.close()
+  closeConnexion(driver)
+  })
+  .catch(function (error) {
+    console.log(error)
+  }); 
+}
+
+
+exports.getPersonVille = function(pays, res) {
+  let driver = createConnexion()
+  let session = driver.session()
+
+  let request = 'MATCH (p:personne)-[:HABITE]->(v:ville), (v)-[:VILLE_DANS]->(pa:pays) where pa.nom = "'+ pays +'" '+
+  'WITH p, v, {nom: p.nom, prenom: p.prenom, ville: v.nom, pays: pa.nom} as x '+
+  'match (p:personne)-[:EST_INFLUENT]->(di1:domaine_influence) '+
+  'match (p:personne)--()-[:EST_INFLUENT]->(di2:domaine_influence) '+
+  'WITH (count(di1) + count(di2)) as totalInfluence, x '+
+  'RETURN x '+
+  'ORDER BY totalInfluence ASC '+
+  'LIMIT 10'
+
+  session
+  .run(request)
+  .then(function (result) {
+    let resultat = {}
+    result.records.forEach(function (record) {
+      const pers = record.get('x')
+      if (!(pers.ville in resultat)) {
+        resultat[pers.ville] = []
+      }
+      resultat[pers.ville].push({
+        nom: pers.nom,
+        prenom: pers.prenom,
+        label: pers.nom + ' ' + pers.prenom
+      })
+    });
+  res(resultat)
+  session.close()
+  closeConnexion(driver)
+  })
+  .catch(function (error) {
+    console.log(error)
+  }); 
+}
   
-  
+exports.getDetailPersonne = function(nom, prenom, res) {
+  let driver = createConnexion()
+  let session = driver.session()
+
+  let request = 'Match (p:personne {nom: "' + nom +'", prenom: "'+ prenom +'"})-[r:EST_AMI_AVEC]->(n:personne) '+
+  'Match (p:personne {nom: "' + nom +'", prenom: "'+ prenom +'"})-[r3:HABITE]->(v:ville) '+
+  'OPTIONAL Match (p:personne {nom: "' + nom +'", prenom: "'+ prenom +'"})-[r1:A_TRAVAILLE_A]->(e:entreprise) '+
+  'OPTIONAL Match (p:personne {nom: "' + nom +'", prenom: "'+ prenom +'"})-[r2:EST_ALLE_A]->(ec:ecole) '+
+  'with collect(n.nom+\' \'+n.prenom) as ne, e, ec, v, p '+
+  'with collect(e.nom) as en, ne, ec, v, p '+
+  'with collect(ec.nom) as eco, ne, en, v, p '+
+  'with collect(v.nom) as villes, eco, ne, en, p '+
+  'with {personnes: ne, entreprises: en, ecoles: eco, villes: villes, label: p.nom+\' \'+p.prenom } as x '+
+  'return x'
+
+  session
+  .run(request)
+  .then(function (result) {
+    let resultat = []
+    result.records.forEach(function (record) {
+      resultat.push(record.get('x'))
+    });
+  res(resultat)
+  session.close()
+  closeConnexion(driver)
+  })
+  .catch(function (error) {
+    console.log(error)
+  }); 
+}
+
+
--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,19 @@ const reqneo4j = require('./requeteNeo4j')
 
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, token, Accept");
+  next();
+});
+
+
+app.options('/*', function (request, response, next) {
+  response.header("Access-Control-Allow-Methods", "GET, POST");
+  response.send();
+});
+
+
 // app.get('/', (request, response) => {
 //   response.send('Hello from Express!')
 // })
@@ -15,10 +28,36 @@ app.use(bodyParser.json())
 //   res.send('ca marche')
 // })
 
+app.get('/getPays', (req, res) => {
+  reqneo4j.getPays((result) => {
+    res.json({
+      result
+    })
+  })
+})
+
+app.post('/getPersonVille', (req, res) => {
+  const pays = req.body.pays
+  reqneo4j.getPersonVille(pays, (result) => {
+    res.json({
+      result
+    })
+  })
+})
+
 app.post('/getPersonRank', (req, res) => {
   const nb = req.body.nb
   reqneo4j.getRecruteur(nb, (result) => {
-    console.log(result)
+    res.json({
+      result
+    })
+  })
+})
+
+app.post('/getDetailPersonne', (req, res) => {
+  const nom = req.body.nom
+  const prenom = req.body.prenom
+  reqneo4j.getDetailPersonne(nom, prenom, (result) => {
     res.json({
       result
     })
